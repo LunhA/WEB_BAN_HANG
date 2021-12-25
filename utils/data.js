0e3d4d082a8517a944027fc00f6f0ef3df83ffa6
Quan Ma <minhquanma@gmail.com>
2021-12-25 21:24:14 +0700

    Updated data.js

--- utils/data.js
+++ utils/data.js
@@ -1,6 +1,41 @@
+const PRODUCT_TYPE = {
+  FULL_SIZED = 'full-sized',
+  IN_EAR = 'in-ear',
+  TRUE_WIRELESS = 'true-wireless',
+}
+
+const PRODUCT_BRANDS = {
+  FOCAL = 'Focal',
+  HIFIMAN = "HiFiMan",
+  MOONDROP = "MOONDROP",
+  SONY = "SONY",
+  APPLE = "Apple",
+}
 const PRODUCTS = [
   {
     id: 1,
-    name: "",
+    name: "HiFiMan HE400SE",
+    price: 3990000,
+    type: PRODUCT_TYPE.FULL_SIZED,
+    brand: '',
+    image: "http://"
+  },
+  {
+    id: 2,
+    name: "Focal Celestee",
+    price: 23000000,
+    type: PRODUCT_TYPE.FULL_SIZED
+  },
+  {
+    id: 3,
+    name: "HiFiMan HE400SE",
+    price: 3990000,
+    type: PRODUCT_TYPE.FULL_SIZED
+  },
+  {
+    id: 4,
+    name: "HiFiMan HE400SE",
+    price: 3990000,
+    type: PRODUCT_TYPE.FULL_SIZED
   },
 ];
